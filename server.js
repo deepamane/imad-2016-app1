e44fa1b5f90a61d67936491b2503c13c3db420c5
deepamane <deepabmane@gmail.com>
2016-09-21 12:33:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.get('/home/apcoer/webapp/imad-2016-app/ui/IMG_0601.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'IMG_0601.png'));
+});
+app.get('/home/apcoer/webapp/imad-2016-app/ui/index.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.jpg'));
+});
+app.get('/home/apcoer/webapp/imad-2016-app/ui/images.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'images.jpg'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
